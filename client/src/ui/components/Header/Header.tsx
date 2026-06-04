ecc492bc82ff5c321a6a2fd86638ad91f6ac663e
binggwa <qudrhks317@gmail.com>
2026-06-05 01:22:15 +0900

    feat: Header가 icon을 받을 수 있게 추가

--- client/src/ui/components/Header/Header.tsx
+++ client/src/ui/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const HeaderWrapper = styled.header`
   background-color: #000000;
   width: 100%;
   height: 64px;
-  padding: 24px;
+  padding: 0 24px;
   display: flex;
   align-items: center;
   color: #ffffff;
@@ -12,22 +12,34 @@ const HeaderWrapper = styled.header`
 
 const Logo = styled.div`
   cursor: pointer;
+  display: flex;
+  align-items: center;
 
   h1 {
     font-size: 20px;
     font-weight: 800;
   }
+
+  img {
+    width: 32px;
+    height: 32px;
+  }
 `;
 
 interface HeaderProps {
   onLogoClick?: () => void;
+  iconSrc?: string;
 }
 
-export const Header = ({ onLogoClick }: HeaderProps) => {
+export const Header = ({ onLogoClick, iconSrc }: HeaderProps) => {
   return (
     <HeaderWrapper>
       <Logo onClick={onLogoClick}>
-        <h1>SHOP</h1>
+        {iconSrc ? (
+          <img src={iconSrc} alt="헤더 아이콘" />
+        ) : (
+          <h1>SHOP</h1>
+        )}
       </Logo>
     </HeaderWrapper>
   );
