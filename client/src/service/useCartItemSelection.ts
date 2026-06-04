ce6893e6e7208e2268269e41c780364c8f4a422c
binggwa <qudrhks317@gmail.com>
2026-06-04 21:10:59 +0900

    feat: 파생상태로 배달비, 선택한 가격, 총 가격 구현

--- client/src/service/useCartItemSelection.ts
+++ client/src/service/useCartItemSelection.ts
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import type { CartItem } from "../domain/Types";
 
+const FREE_DELIVERY_LIMIT = 100000;
+const DELIVERY_PRICE = 3000;
+
 export const useCartItemSelection = (cartItems: CartItem[]) => {
   const [selectedIds, setSelectedIds] = useState<number[]>(() => {
     const savedSelection = localStorage.getItem("cart_item_selection");
@@ -35,9 +38,28 @@ export const useCartItemSelection = (cartItems: CartItem[]) => {
     }
   };
 
+  const selectedItems = cartItems.filter((item) => selectedIds.includes(item.cartItemId));
+  const isAllSelected = cartItems.length > 0 && selectedIds.length === cartItems.length;
+
+  const totalProductPrice = selectedItems.reduce((sum, item) => sum + item.product.price * item.quantity, 0);
+
+  const calculateDeliveryPrice = () => {
+    if (totalProductPrice === 0) return 0;
+    if (totalProductPrice >= FREE_DELIVERY_LIMIT) return 0;
+    return DELIVERY_PRICE;
+  }
+
+  const deliveryPrice = calculateDeliveryPrice();
+
+  const totalPrice = totalProductPrice + deliveryPrice;
+
   return {
     selectedIds,
     toggleSelection,
     toggleAll,
+    isAllSelected,
+    totalProductPrice,
+    deliveryPrice,
+    totalPrice,
   };
 };
