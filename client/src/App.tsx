b1d113fe7153b23777f05b032de489562150e564
binggwa <qudrhks317@gmail.com>
2026-06-04 13:39:52 +0900

    chore: 환경설정 vite, react, ts, emotion, msw, storybook

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+`;
+
+function App() {
+  return (
+    <AppContainer>
+      <h1>초기설정</h1>
+    </AppContainer>
+  );
+}
+
+export default App;
