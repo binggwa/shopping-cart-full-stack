6b04d7f42e6fc849cf6b6da8959b0d74ad030a72
binggwa <qudrhks317@gmail.com>
2026-06-08 13:18:07 +0900

    chore: 주석처리 이유 추가

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {App} from "./App.tsx";
 
+// 배포 시 msw가 실제 api 요청을 가로채지 않도록 임시 주석처리
 // async function enableMocking() {
 //   if (!import.meta.env.DEV) {
 //     return;
