6aab0817fefb1dd5b3b08b92d38e7e587f33c471
binggwa <qudrhks317@gmail.com>
2026-06-19 16:42:36 +0900

    feat: cart 페이지에서 주문제출시 라우팅 경로 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Global } from '@emotion/react';
 import { CartPage } from './ui/pages/CartPage/CartPage';
-import { OrderConfirmPage } from './ui/pages/OrderConfirmPage/OrderConfirmPage';
 import { resetStyles } from './styles/resetStyles';
+import { PreorderPage } from './ui/pages/PreorderPage/PreorderPage';
 
 export const App = () => {
   return (
@@ -11,7 +11,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/cart" replace />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/order-confirm" element={<OrderConfirmPage />} />
+        <Route path="/preorder" element={<PreorderPage />} />
         <Route path="*" element={<Navigate to="/cart" replace />} />
       </Routes>
     </BrowserRouter>
