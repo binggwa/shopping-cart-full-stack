b2d81f94285eebf409900553ce6657919a07bc96
binggwa <qudrhks317@gmail.com>
2026-06-20 19:12:01 +0900

    feat: OrderConfirmPage 수정 및 라우팅 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Global } from '@emotion/react';
 import { CartPage } from './ui/pages/CartPage/CartPage';
 import { resetStyles } from './styles/resetStyles';
 import { PreorderPage } from './ui/pages/PreorderPage/PreorderPage';
+import { OrderConfirmPage } from './ui/pages/OrderConfirmPage/OrderConfirmPage';
 
 export const App = () => {
   return (
@@ -12,6 +13,7 @@ export const App = () => {
         <Route path="/" element={<Navigate to="/cart" replace />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/preorder" element={<PreorderPage />} />
+        <Route path="/orders/:orderId" element={<OrderConfirmPage />} />
         <Route path="*" element={<Navigate to="/cart" replace />} />
       </Routes>
     </BrowserRouter>
