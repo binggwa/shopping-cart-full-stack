10f5374ef04f2647bc0c69881695ea91c1534b84
binggwa <qudrhks317@gmail.com>
2026-06-25 16:48:06 +0900

    fix: alert가 두번뜨던 문제 해결을 위해 alert를 set 밖으로 분리

--- client/src/ui/pages/PreorderPage/useCouponModal.ts
+++ client/src/ui/pages/PreorderPage/useCouponModal.ts
@@ -12,14 +12,17 @@ export const useCouponModal = (selectedCouponIds: number[]) => {
   };
 
   const toggleTempCoupon = (couponId: number) => {
-    setTempSelectedCouponIds((prev) => {
-      if (prev.includes(couponId)) return prev.filter((id) => id !== couponId);
-      if (prev.length >= 2) {
-        alert("쿠폰은 최대 2개까지 사용할 수 있습니다!!");
-        return prev;
-      }
-      return [...prev, couponId];
-    });
+    if (tempSelectedCouponIds.includes(couponId)) {
+      setTempSelectedCouponIds((prev) => prev.filter((id) => id !== couponId));
+      return;
+    }
+
+    if (tempSelectedCouponIds.length >= 2) {
+      alert("쿠폰은 최대 2개까지 사용할 수 있습니다.");
+      return;
+    }
+
+    setTempSelectedCouponIds((prev) => [...prev, couponId]);
   };
 
   return {
